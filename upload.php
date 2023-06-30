523738152a0a99670dbb0b721394de49b99909ae
Fatlinda <fatlindabrahaj@gmail.com>
2023-06-30 23:43:43 +0200

    enctype

---
+++ upload.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+
+
+</head>
+<body>
+
+</body>
+<form action ="upload.php" enctype="multipart/form-data" method="post">
+    <input type ="file" name="file_upload"><br>
+
+    <input type="submit"name="submit">
+</form>
+
+
+
+</html>
+
