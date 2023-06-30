8d3ec6d6cce33f7b62f3ccd393eedc058d6cbc5d
Fatlinda <fatlindabrahaj@gmail.com>
2023-07-01 00:04:03 +0200

    errors

--- upload.php
+++ upload.php
@@ -1,5 +1,31 @@
 <?php
 
+if(isset($_POST['submit'])){
+
+
+    print_r($_FILES['file_upload']);
+}
+
+$upload_errors=array(
+        UPLOAD_ERR_OK => "THERE IS NO ERROR",
+        UPLOAD_ERR_INI_SIZE=> "EXCEED THE SIZE",
+        UPLOAD_ERR_FORM_SIZE=> "EXCEED THE MAX_FILE_SIZE",
+        UPLOAD_ERR_PARTIAL=>"PARTIALLY UPLOADED",
+        UPLOAD_ERR_NO_FILE=>"NO FILE UPLOADED",
+        UPLOAD_ERR_NO_TMP_DIR=>"MISSING TEMPRORAY FOLDER",
+        UPLOAD_ERR_CANT_WRITE=>"FAILD TO WRITE FILE TO DISK",
+        UPLOAD_ERR_EXTENSION=>"A PHP EXTENSION STOPPED THE FILE UPLOAD"
+
+
+
+
+);
+
+$the_error=$_FILES['file_upload']['error'];
+
+$the_message= $upload_errors[$the_error];
+
+
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +40,13 @@
 
 </body>
 <form action ="upload.php" enctype="multipart/form-data" method="post">
-    <input type ="file" name="file_upload"><br>
+  <?php if(!empty($upload_errors)){
+
+        echo $the_message."<br>";
+
+   }
+?>
+<input type ="file" name="file_upload"><br>
 
     <input type="submit"name="submit">
 </form>
